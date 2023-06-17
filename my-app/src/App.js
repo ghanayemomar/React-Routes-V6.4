00e867618ef061b697f961498b918f79890fe720
ghanayemomar <o.ghanaiem@student.aaup.edu>
2023-06-18 02:03:30 +0300

    Layouts & Nested Routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProdutsPage from "./pages/Products";
-
+import RootLayout from "./pages/Root";
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "products", element: <ProdutsPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "products", element: <ProdutsPage /> },
+    ],
+  },
 ]);
 function App() {
   return <RouterProvider router={router} />;
 }
 
 export default App;
- 
--- my-app/src/pages/Products.js
+++ my-app/src/pages/Products.js
@@ -1,6 +1,4 @@
 function ProdutsPage() {
-  return (
-      <h1>My Products Page.</h1>
-  );
+  return <h1>My Products Page.</h1>;
 }
 export default ProdutsPage;
